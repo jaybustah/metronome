5377cb4ae03fcd778bb63ece9d5ad718d7156992
Jay Guerrero <jayguerrero@Jay-Guerreros-MacBook-Pro.local>
2018-04-04 02:38:54 -0400

    progress

--- Knob-master/Knob/ViewController.swift
+++ Knob-master/Knob/ViewController.swift
@@ -2,24 +2,75 @@
 
 import UIKit
 import AVFoundation
+import AudioToolbox
 
 class ViewController: UIViewController {
-    @IBOutlet weak var knob: Knob!
-    @IBOutlet weak var BPM_label: UILabel!
+    @IBOutlet weak var knob: Knob! 
+    @IBOutlet weak var BPMlabel: UILabel!
     @IBOutlet weak var playButton: UIButton!
-    
-    /////
-    var metronomeTimer: Timer!
-    var metronomeIsOn = false
-    var metronomeSoundPlayer: AVAudioPlayer!
-    var metronomeAccentPlayer: AVAudioPlayer!
-    /////
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var tempoStepper: UIStepper!
+    @IBOutlet weak var tsUpperLabel: UILabel!
+    @IBOutlet weak var tsLowerLabel: UILabel!
+    @IBOutlet weak var tsUpperStepper: UIStepper!
+    @IBOutlet weak var tsLowerStepper: UIStepper!
     
     var bpm: Double = 90
     var lastPos: Double = 0
     
+    var metronomeTimer: Timer!
+    var metronomeIsOn = false
+    var metronomeSoundPlayer: AVAudioPlayer!
+    var metronomeAccentPlayer: AVAudioPlayer!
+    
+    var count: Int = 0 {
+        didSet {
+            if count == 0 {
+                countLabel.text = String(tsUpper)
+            } else {
+                countLabel.text = String(count)
+            }
+        }
+    }
+   
+    var tempo: TimeInterval = 90 {
+        didSet {
+            //BPMlabel.text = String(format: "%.0f", bpm)
+            tempoStepper.value = bpm
+        }
+    }
+ 
+    var tsUpper: Int = 4 {
+        didSet {
+            tsUpperLabel.text = String(tsUpper)
+            tsUpperStepper.value = Double(tsUpper)
+        }
+    }
+    var tsLower: Int = 4 {
+        didSet {
+            tsLowerLabel.text = String(tsLower)
+            tsLowerStepper.value = Double(tsLower)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tempoStepper.autorepeat = true
+        //bpm = 90
+        tsUpper = 4
+        tsLower = 4
+       
+        
+        let metronomeSoundURL = URL(fileURLWithPath: Bundle.main.path(forResource: "snizzap1", ofType: "wav")!)
+        let metronomeAccentURL = URL(fileURLWithPath: Bundle.main.path(forResource: "snizzap2", ofType: "wav")!)
+        
+        metronomeSoundPlayer = try? AVAudioPlayer(contentsOf: metronomeSoundURL)
+        metronomeAccentPlayer = try? AVAudioPlayer(contentsOf: metronomeAccentURL)
+        
+        metronomeSoundPlayer.prepareToPlay()
+        metronomeAccentPlayer.prepareToPlay()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -28,35 +79,83 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /*
-    @IBAction func toggleClick(_ sender: UIButton!) {
-        if metronomeIsOn {
-      //      stopMetronome(sender)
+    @IBAction func stepTempo(_ sender: UIStepper) {
+        //BPMlabel.text = "\(Double(sender.value))"
+        //bpm += sender.value
+        //bpm += tempoStepper.value
+        var value = tempoStepper.value
+        
+        if (tempoStepper.value > value) {
+            value += 1
         } else {
-        //    startMetronome(sender)
+            value -= 1
         }
         
-        func stopMetronome(_ sender: UIButton) {
-            metronomeIsOn = false
-            metronomeTimer?.invalidate()
-            //count = 0
-            sender.setTitle("Start", for: UIControlState())
-        }
+        BPMlabel?.text = (String(format: "%.0f", bpm + value))
         
-        func startMetronome(_ sender: UIButton) {
-            metronomeIsOn = true
-            sender.setTitle("Stop", for: UIControlState())
-            let metronomeTimeInterval:TimeInterval = (240.0 / Double(tsLower)) / bpm
-            metronomeTimer = Timer.scheduledTimer(timeInterval: metronomeTimeInterval, target: self, selector: #selector(playMetronomeSound), userInfo: nil, repeats: true)
-            metronomeTimer?.fire()
-        }
- 
-        
+        restartMetronome()
     }
-*/
+   
+    
+    @IBAction func stepTsUpper(_ sender: UIStepper) {
+        tsUpper = Int(sender.value)
+        
+        restartMetronome()
+    }
+    
+    
+    @IBAction func stepTsLower(_ sender: UIStepper) {
+        tsLower = Int(sender.value)
+        restartMetronome()
+    }
+    
+    
+    @IBAction func toggleClick(_ sender: UIButton) {
+        if metronomeIsOn {
+            stopMetronome(sender)
+        } else {
+            startMetronome(sender)
+        }
+    }
+  
+    func stopMetronome(_ sender: UIButton) {
+        metronomeIsOn = false
+        metronomeTimer?.invalidate()
+        count = 0
+        sender.setTitle("Start", for: UIControlState())
+    }
+    
+    func startMetronome(_ sender: UIButton) {
+        metronomeIsOn = true
+        sender.setTitle("Stop", for: UIControlState())
+        let metronomeTimeInterval:TimeInterval = (240.0 / Double(tsLower)) / bpm
+        metronomeTimer = Timer.scheduledTimer(timeInterval: metronomeTimeInterval, target: self, selector: #selector(playMetronomeSound), userInfo: nil, repeats: true)
+        metronomeTimer?.fire()
+    }
+    
+    func playMetronomeSound() {
+        count += 1
+        if count == 1 {
+            metronomeAccentPlayer.play()
+        } else {
+            metronomeSoundPlayer.play()
+            if count == tsUpper {
+                count = 0
+            }
+        }
+    }
+    
+    func restartMetronome() {
+        if metronomeIsOn {
+            stopMetronome(playButton)
+            startMetronome(playButton)
+        }
+    }
+
+    
     @IBAction func knobRotated(_ sender: Knob) {
         super.viewDidLoad()
-        
+     
         var diff: Double
         diff = (knob.value) - lastPos
         
@@ -71,14 +170,14 @@ class ViewController: UIViewController {
         lastPos = knob.value
         bpm += diff * 10
         
-        var feedbackGenerator : UISelectionFeedbackGenerator? = nil
         
         if(bpm < 30)
         {
             bpm = 30
             knob.tintColor = UIColor.red
             knob.isEnabled = false
-                        generator.notificationOccurred(.error)
+       
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
             //knob.backgroundColor = UIColor(white: 0.0, alpha: 1.0)
         }
         else if (bpm < 400 && bpm > 30)
@@ -92,27 +191,19 @@ class ViewController: UIViewController {
             bpm = 400
             knob.tintColor = UIColor.red
             knob.isEnabled = false
-            
-            generator.notificationOccurred(.error)
-            //knob.backgroundColor = UIColor(white: 0.0, alpha: 1.0)
+           //knob.backgroundColor = UIColor(white: 0.0, alpha: 1.0)
         }
         
-        print("value: \(knob.value)")
+        print("value: \(knob.value)") // console output
+        
+       
         if 30...400 ~= bpm {
-        BPM_label?.text = (String(format: "%.0f", bpm)) // zero decimal places
+        BPMlabel?.text = (String(format: "%.0f", bpm))
         }
-        
-        
- //
- let metronomeSoundURL = URL(fileURLWithPath: Bundle.main.path(forResource: "snizzap1", ofType: "wav")!)
- let metronomeAccentURL = URL(fileURLWithPath: Bundle.main.path(forResource: "snizzap2", ofType: "wav")!)
- /*
- metronomeSoundPlayer = try? AVAudioPlayer(contentsOf: metronomeSoundURL)
- metronomeAccentPlayer = try? AVAudioPlayer(contentsOf: metronomeAccentURL)
- 
- metronomeSoundPlayer.prepareToPlay()
- metronomeAccentPlayer.prepareToPlay()
- */
-    }
+        restartMetronome()
+
+  
+        }
+    
 }
 
